81454ce6654d67a68fda87821992768b739bfa1c
Albogast PC <albogasty@gmail.com>
2023-03-28 13:00:53 +0300

    updated all user dashboards to view his/her work only

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,12 +36,22 @@ class HomeController extends Controller
         $date = "'".date('Y-m-d')."'";
         $where_date = request('days') > 0 ? date('Y-m-d', strtotime('- '.request('days').'days')) : date('Y-m-01');
         $where_ = request('days') > 0 ? "'".date('Y-m-d', strtotime('- '.request('days').'days'))."'" : "'".date('Y-m-01')."'";
-        $payments = DB::select('WITH alltasks as(SELECT c.id, c.name, sum(b.amount) as total, COUNT(DISTINCT b.client_id) as client FROM payments b JOIN clients a on b.client_id=a.id join partners c on c.id=a.partner_id where b.created_at >='.$where_.' GROUP BY c.id, c.name), allusers as (SELECT a.id, a.name, sum(b.amount) as amount, COUNT(b.id) as clients FROM clients b JOIN partners a on b.partner_id=a.id where b.status=1 GROUP BY a.id, a.name) select a.id, a.name, a.total, a.client, b.amount, b.clients from alltasks a left join allusers b on a.id=b.id order by total desc;');
+        
+        if(Auth::User()->role_id < 3){
+            $where_condition = '';
+        }elseif(Auth::User()->role_id == 3){
+            $where_condition = " AND a.partner_id in (SELECT id from partners where user_id =".Auth::User()->id .") ";
+        }else{
+            $user= \App\Models\User::where('id', Auth::User()->id)->first();
+            $where_condition = " AND a.user_id=$user->id ";
+        }
+
+        $payments = DB::select('WITH alltasks as(SELECT c.id, c.name, sum(b.amount) as total, COUNT(DISTINCT b.client_id) as client FROM payments b JOIN clients a on b.client_id=a.id join partners c on c.id=a.partner_id where b.created_at >='.$where_.$where_condition.' GROUP BY c.id, c.name), allusers as (SELECT b.id, b.name, sum(a.amount) as amount, COUNT(a.id) as clients FROM clients a JOIN partners b on a.partner_id=b.id where a.status=1 '.$where_condition.' GROUP BY b.id, b.name) select a.id, a.name, a.total, a.client, b.amount, b.clients from alltasks a left join allusers b on a.id=b.id order by total desc;');
         return inertia('Dashboard/Payment',
         [
             'partners' => $payments,
-            'collections' => DB::select('SELECT b.uuid, a.name as collector, a.uuid as user_id, b.client_id, c.name, c.account, c.branch, c.phone, b.amount as amount, b.date FROM payments b JOIN users a on b.user_id=a.id join clients c on c.id=b.client_id ORDER BY a.id desc limit 20;'),
-            'payments' => DB::select('SELECT a.id, a.name, a.code, count(b.id) as total, sum(b.amount) as amount, COUNT(DISTINCT b.client_id) as client FROM payments b JOIN users a on b.user_id=a.id WHERE date <='.$date.' GROUP BY a.id, a.name, a.code ORDER BY sum(b.amount) desc')
+            'collections' => DB::select('SELECT b.uuid, c.name as collector, c.uuid as user_id, b.client_id, a.name, a.account, a.branch, a.phone, b.amount as amount, b.date FROM payments b JOIN users c on b.user_id=c.id join clients a on a.id=b.client_id ORDER BY c.id desc limit 20;'),
+            'payments' => DB::select('SELECT c.id, c.name, c.code, count(b.id) as total, sum(b.amount) as amount, COUNT(DISTINCT b.client_id) as client FROM payments b JOIN users c on b.user_id=c.id JOIN clients a on b.client_id=a.id  WHERE date <='.$date.$where_condition.' GROUP BY c.id, c.name, c.code ORDER BY sum(b.amount) desc')
 
         ]);
     }
@@ -344,8 +354,17 @@ public function calendar_data($id = null){
             $title = 'Collections from '. $partner->name.' Since ';
 
         }else{
-            $where_condition = '';
-        }
+            if(Auth::User()->role_id < 3){
+                $where_condition = '';
+            }elseif(Auth::User()->role_id == 3){
+                $where_condition = " AND a.partner_id in (SELECT id from partners where user_id =".Auth::User()->id .") ";
+                $title = 'All Staff';
+            }else{
+                $user= \App\Models\User::where('id', Auth::User()->id)->first();
+                $title = 'Collections of '.$user->name.' Since ';
+                $where_condition = " AND f.id=$user->id ";
+            }
+        } 
         $date = "'".date('Y-m-d')."'";
         $where_date = request('days') > 0 ? date('Y-m-d', strtotime('- '.request('days').'days')) : (request('days') != '' ? date('Y-m-d') : date('Y-m-01'));
         $where_ = request('days') > 0 ? "'".date('Y-m-d', strtotime('- '.request('days').'days'))."'" :  (request('days') != '' ? "'".date('Y-m-d')."'" : "'".date('Y-m-01')."'");
